c610e80c68bec0b0578d7425dbf8dc3b5f6cbfdc
Ana Vitoria Apolinario de Queiroz <ana.queiroz89@etec.sp.gov.br>
2026-03-02 00:24:20 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras1/Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using MauiAppMinhasCompras1.Models;
+
+namespace MauiAppMinhasCompras1.Helpers
+{
+    internal class SQLiteDatabaseHelper
+    {
+    }
+}
